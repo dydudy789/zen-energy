52bb8e575f3bb92ae45dbeb28123a1f0668186a1
dowkwon lee <dydudy78@gmail.com>
2026-03-28 14:24:42 +1100

    Read raw data

--- notebooks/ingest_and_model.py
+++ notebooks/ingest_and_model.py
@@ -10,5 +10,35 @@ spark = (
 )
 
 
+# Read CSVs
+dispatch_intervals_df = (
+    spark.read
+    .option("header", True)
+    .option("inferSchema", True)
+    .csv("nem_data/raw_dispatch_intervals.csv")
+)
+
+unit_dispatch_df = (
+    spark.read
+    .option("header", True)
+    .option("inferSchema", True)
+    .csv("nem_data/raw_unit_dispatch.csv")
+)
+
+reference_generators_df = (
+    spark.read
+    .option("header", True)
+    .option("inferSchema", True)
+    .csv("nem_data/reference_generators.csv")
+)
+
+print("Raw Dispatch Intervals Data")
+dispatch_intervals_df.show(10, truncate=False)
+
+print("Raw Unit Dispatch Data")
+unit_dispatch_df.show(10, truncate=False)
+
+print("Reference Generator data")
+reference_generators_df.show(10, truncate=False)
 
 spark.stop()
